2025e75b2c0e8e63b2f01143f90633cc9b900dd4
Angelicafredie <146182477+Angelicafredie@users.noreply.github.com>
2025-07-29 17:49:16 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    /*==================== sticky navbar ====================*/
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+};
+
+
+
+
+/*==================== typed js ====================*/
